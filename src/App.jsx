c750a493d6e973ca03f734d912a6d2ccdcefb555
rpc404 <theboringschool.org@gmail.com>
2023-03-14 12:12:01 +0545

    Wallet Connection Success

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Navbar from "./components/Navbar";
+import WalletConnectSetup, {
+  ethereumClient,
+  projectId,
+} from "./components/WalletConnect";
 
 import React from "react";
 import { LazyMotion, domAnimation } from "framer-motion";
 import { Web3Modal } from "@web3modal/react";
-import WalletConnectSetup, { ethereumClient } from "./components/WalletConnect";
 
 export default function App() {
   return (
@@ -14,8 +17,17 @@ export default function App() {
         </LazyMotion>
       </WalletConnectSetup>
       <Web3Modal
-        projectId="9b235f5f681cdcc9568adb7b291f36a3"
+        projectId={projectId}
         ethereumClient={ethereumClient}
+        themeVariables={{
+          "--w3m-font-family": "Oswald, sans-serif",
+          "--w3m-background-color": "#000",
+          "--w3m-accent-color": "#000",
+          "--w3m-logo-image-url":
+            "https://ik.imagekit.io/domsan/Logo_0vBSw9piY.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1662803005580",
+          "--w3m-text-medium-regular-size": "18px",
+          "--w3m-text-medium-regular-text-transform": "uppercase",
+        }}
       />
     </>
   );
--- src/components/WalletConnect/index.jsx
+++ src/components/WalletConnect/index.jsx
@@ -5,10 +5,10 @@ import {
   w3mProvider,
 } from "@web3modal/ethereum";
 import { configureChains, createClient, WagmiConfig } from "wagmi";
-import { arbitrum, mainnet, polygon } from "wagmi/chains";
+import { arbitrum, sepolia } from "wagmi/chains";
 
-const chains = [arbitrum, mainnet, polygon];
-const projectId = "YOUR_PROJECT_ID";
+const chains = [arbitrum, sepolia];
+export const projectId = "9b235f5f681cdcc9568adb7b291f36a3";
 
 const { provider } = configureChains(chains, [w3mProvider({ projectId })]);
 const wagmiClient = createClient({
@@ -19,5 +19,5 @@ const wagmiClient = createClient({
 export const ethereumClient = new EthereumClient(wagmiClient, chains);
 
 export default function WalletConnectSetup({ children }) {
-  return <WagmiConfig client={wagmiClient}>{...children}</WagmiConfig>;
+  return <WagmiConfig client={wagmiClient}>{children}</WagmiConfig>;
 }
